e3bb23ff6401988f2ca29d63dfcd36c16b325374
Danut Striblea <dan_22st@yahoo.com>
2025-02-08 20:44:05 +0200

    Corect5

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/repository-name/", // Înlocuiește 'repository-name' cu numele repository-ului tău
+  base: "/dan-store/", // Înlocuiește 'repository-name' cu numele repository-ului tău
 });

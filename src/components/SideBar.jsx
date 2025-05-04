dd7785028896bda62dc829d27a178d48cba2db60
Danut Striblea <dan_22st@yahoo.com>
2025-05-04 11:45:14 +0300

    modificat sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -6,35 +6,36 @@ import { FaStar } from "react-icons/fa";
 const SideBar = ({ isSidebarOpen, toggleSidebar }) => {
   const navigate = useNavigate();
   const [genderFilter, setGenderFilter] = useState("");
-  const [priceRange, setPriceRange] = useState([1, 1200]);
+  // Folosim o stare pentru prețul maxim; minimul este fixat la 1 RON
+  const [maxPrice, setMaxPrice] = useState(5000);
   const [rating, setRating] = useState(0);
+
+  const updateFilters = (gender, maxPrice, rating) => {
+    navigate(
+      `/product-list?gender=${gender}&minPrice=1&maxPrice=${maxPrice}&rating=${rating}`
+    );
+  };
 
   const handleGenderFilterClick = (gender) => {
     if (genderFilter === gender) {
       setGenderFilter("");
-      updateFilters("", priceRange, rating);
+      updateFilters("", maxPrice, rating);
     } else {
       setGenderFilter(gender);
-      updateFilters(gender, priceRange, rating);
+      updateFilters(gender, maxPrice, rating);
     }
   };
 
   const handlePriceChange = (e) => {
-    const value = e.target.value.split(",").map(Number);
-    setPriceRange(value);
-    updateFilters(genderFilter, value, rating);
+    const newValue = Number(e.target.value);
+    setMaxPrice(newValue);
+    updateFilters(genderFilter, newValue, rating);
   };
 
   const handleRatingChange = (e) => {
     const newRating = parseInt(e.target.value, 10);
     setRating(newRating);
-    updateFilters(genderFilter, priceRange, newRating);
-  };
-
-  const updateFilters = (gender, price, rating) => {
-    navigate(
-      `/product-list?gender=${gender}&minPrice=${price[0]}&maxPrice=${price[1]}&rating=${rating}`
-    );
+    updateFilters(genderFilter, maxPrice, newRating);
   };
 
   const handleOverlayClick = (e) => {
@@ -52,74 +53,83 @@ const SideBar = ({ isSidebarOpen, toggleSidebar }) => {
         ></div>
       )}
       <nav
-        className={`fixed md:static inset-y-0 left-0 bg-stone-800 text-white 
-        p-4 transition-transform duration-300 
-      ease-in-out z-30 top-[6.5rem] bottom-[4.5rem] w-64 md:w-1/4 lg:w-1/5 xl:w-1/6 ${
-        isSidebarOpen ? "translate-x-0" : "-translate-x-full md:translate-x-0"
-      }`}
+        style={{ boxShadow: "4px 0 8px rgba(0, 0, 0, 0.3)" }}
+        className={`fixed md:static inset-y-0 left-0 bg-gray-900 text-white p-4 transition-transform duration-300 ease-in-out z-30 top-[6.5rem] bottom-[4.5rem] w-64 md:w-1/4 lg:w-1/5 xl:w-1/6 ${
+          isSidebarOpen ? "translate-x-0" : "-translate-x-full md:translate-x-0"
+        }`}
       >
         {/* Sidebar content */}
         <div className="relative z-10 h-full">
-          {/* Close button for mobile */}
+          {/* Butonul de închidere pentru mobile, mărit și repoziționat */}
           <button
-            className="absolute top-4 right-4 md:hidden"
+            className="absolute -top-2 right-0 md:hidden text-xl text-gray-400 hover:text-gray-50"
             onClick={toggleSidebar}
           >
             ✕
           </button>
 
-          <ul className="space-y-2">
-            {/* Butoane din dropdown */}
+          <ul className="space-y-4">
             <li>
-              <p className="text-gray-500 font-bold py-2">Filtre:</p>
-              <div className="block py-2 px-3 text-gray-500 rounded">
-                Categorie:
-              </div>
-              <ul className="pl-4 space-y-2">
-                <li>
+              <p className="text-gray-400 font-bold pb-2 border-b border-gray-700">
+                Filtre
+              </p>
+              <div className="mt-4">
+                <p className="text-sm text-gray-400 mb-1">Categorie:</p>
+                <div className="flex space-x-2">
                   <button
-                    className={`block py-2 px-3 text-white hover:scale-105 hover:bg-gray-900 active:scale-110 rounded ${
-                      genderFilter === "masculin" ? "bg-gray-900" : ""
-                    }`}
                     onClick={() => handleGenderFilterClick("masculin")}
-                  >
-                    Barbati
-                  </button>
-                </li>
-                <li>
-                  <button
-                    className={`block py-2 px-3 text-white hover:scale-105 hover:bg-gray-900 active:scale-110 rounded ${
-                      genderFilter === "feminin" ? "bg-gray-900" : ""
+                    className={`py-2 px-3 rounded transition-transform duration-200 ${
+                      genderFilter === "masculin"
+                        ? "scale-105 bg-sky-900"
+                        : "scale-100 bg-gray-700 md:bg-gray-800 md:hover:bg-gray-700"
                     }`}
+                  >
+                    Bărbați
+                  </button>
+
+                  <button
                     onClick={() => handleGenderFilterClick("feminin")}
+                    className={`py-2 px-3 rounded transition-transform duration-200 ${
+                      genderFilter === "feminin"
+                        ? "scale-105 bg-sky-900"
+                        : "scale-100 bg-gray-700 md:bg-gray-800 md:hover:bg-gray-700"
+                    }`}
                   >
                     Femei
                   </button>
-                </li>
-              </ul>
+                </div>
+              </div>
             </li>
-            {/* // interval de pret */}
-            <li className="block py-2 px-3 text-white">
-              <label htmlFor="priceRange">Preț:</label>
+            <li>
+              <label
+                htmlFor="priceRange"
+                className="text-sm text-gray-400 block mb-1 mt-10"
+              >
+                Preț
+              </label>
               <input
                 type="range"
                 id="priceRange"
                 name="priceRange"
                 min="1"
                 max="1200"
-                value={priceRange}
+                value={maxPrice}
                 onChange={handlePriceChange}
-                className="w-full"
-                multiple
+                className="w-full accent-sky-600"
               />
-              <div className="flex justify-between text-xs">
-                <span>{priceRange[0]} RON</span>
-                <span>{priceRange[1]} RON</span>
+              <div className="flex justify-between text-xs mt-1">
+                <span>1 RON</span>
+                <span>{maxPrice} RON</span>
               </div>
             </li>
-            <li className="block py-2 px-3 text-white">
-              <label htmlFor="rating1">Rating:</label>
-              <div id="rating" className="flex flex-wrap space-x-1">
+            <li>
+              <label
+                htmlFor="rating"
+                className="text-sm text-gray-400 block mb-1 mt-10"
+              >
+                Rating:
+              </label>
+              <div id="rating" className="flex space-x-1">
                 {[1, 2, 3, 4, 5].map((star) => (
                   <label key={star} htmlFor={`rating${star}`}>
                     <input
@@ -132,8 +142,8 @@ const SideBar = ({ isSidebarOpen, toggleSidebar }) => {
                       className="hidden"
                     />
                     <FaStar
-                      className={`w-6 h-6 cursor-pointer ${
-                        rating >= star ? "text-yellow-500" : "text-gray-400"
+                      className={`w-6 h-6 cursor-pointer transition-transform duration-200 ${
+                        rating >= star ? "text-yellow-500" : "text-gray-600"
                       }`}
                     />
                   </label>
@@ -147,7 +157,6 @@ const SideBar = ({ isSidebarOpen, toggleSidebar }) => {
   );
 };
 
-// Validare Props
 SideBar.propTypes = {
   isSidebarOpen: PropTypes.bool.isRequired,
   toggleSidebar: PropTypes.func.isRequired,
